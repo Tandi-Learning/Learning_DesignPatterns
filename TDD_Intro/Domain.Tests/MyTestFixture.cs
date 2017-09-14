5eaf4459607ecbe1d74beadb88ba983b7f0a5fe2
Tandi Sunarto <tandi.sunarto@hotmail.com>
2017-09-14 17:58:58 -0400

    Init intro to TDD lesson

---
+++ TDD_Intro/Domain.Tests/MyTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Tests
+{
+    [TestFixture]
+    public class MyTestFixture
+    {
+        [Test]
+        public void MyFirstTest()
+        {
+            var result = 2 + 2;
+
+            Assert.AreEqual(4, result);
+        }
+    }
+}
